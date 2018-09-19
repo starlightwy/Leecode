6aabb52b74a3124177af1420bcbdccfa3312d895
Yue Wang <starlightwy@gmail.com>
2018-09-19 21:49:29 +0200

    Configuration finished.

---
+++ src/ArrayTests.java
@@ -0,0 +1,13 @@
+public class ArrayTests {
+    public static String addBinary(String a, String b) {
+        String result = ""+'1'+'a';
+        System.out.println(result.valueOf('a'));
+        return result;
+    }
+
+    public static void changeLine() {
+        int a = 1;
+        if(a==1)  for(int i=0;i<2;i++)  a += i;
+        System.out.println(a);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        ArrayTests.addBinary("11", "1");
+        ArrayTests.changeLine();
+    }
+}
