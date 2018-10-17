7c20fb3d6f73333db2dedb06b103a2ea7b0b58ae
Yue Wang <starlightwy@gmail.com>
2018-10-17 22:49:06 +0200

    -ContainerWithMostWater

--- src/array/ContainerWithMostWater.java
+++ src/array/ContainerWithMostWater.java
@@ -14,6 +14,7 @@ public class ContainerWithMostWater {
     }
 
     public int solution(int[] height) {
+        //每次只往里面移动较短的边计算
         int maxarea = 0, l = 0, r = height.length - 1;
         while (l < r) {
             maxarea = Math.max(maxarea, Math.min(height[l], height[r]) * (r - l));
